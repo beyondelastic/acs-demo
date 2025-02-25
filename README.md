bfbf22981b9fd5da1e97174758b25582b5687361
beyondelastic <alexander.ullah@googlemail.com>
2025-02-25 16:25:17 +0100

    smsapp added

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ npm install --save @azure/communication-common @azure/communication-calling
 npm install --save-dev parcel
 npx parcel index.html
 ```
+
+## SMS Quickstart
+
+```
+cd SmsQuickstart
+dotnet build
+dotnet add package Azure.Communication.Sms --version 1.0.0-beta.3
+dotnet run
+```
---
+++ SmsQuickstart/Program.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Communication;
+using Azure.Communication.Sms;
+
+// initialize an SmsClient with your connection string.
+
+string connectionString = <your-connection-string>;
+SmsClient smsClient = new SmsClient(connectionString);
+
+// Replace <leased-phone-number> with an SMS-enabled phone number you provisioned via ACS, and <to-phone-number> with the phone number you wish to send a message to.
+
+smsClient.Send(
+   from: new PhoneNumber("<leased-phone-number>"),
+   to: new PhoneNumber("<to-phone-number>"),
+   message: "Hello World via SMS"
+);
